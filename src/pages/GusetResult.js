d12869c3ee4fb3fc81a7d145e233b59a607261b2
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-03-27 15:57:10 +0900

    post 수정

--- src/pages/GusetResult.js
+++ src/pages/GusetResult.js
@@ -49,12 +49,27 @@ const GuestResult = () => {
     comment: "",
   });
 
+  useEffect(() => {
+    API.post("/guest-result", {
+      nickname: nickname,
+      result: state.result,
+      accuracy: state.accuracy,
+      guestId: guestId,
+      role: role,
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          console.log("success");
+        }
+      })
+      .catch((error) => console.log(error.res));
+  }, []);
+
   const handleSend = () => {
     setIsLoading(true);
     API.post("/guest-result", {
       nickname: nickname,
       result: state.result,
-      // todo : accuracy가 항상 0 값입니다. 수정해야 할 것 같아요
       accuracy: state.accuracy,
       comment: state.comment,
       guestId: guestId,
